084bf6143b5a2d6504036264d525c88d696f14b3
jiyunbaik <58647409+jiyunbaik@users.noreply.github.com>
2019-12-08 00:12:03 -0500

    Add files via upload

---
+++ BattleShip/Battleship/src/battleship/Cell.java
@@ -0,0 +1,16 @@
+
+package battleship;
+
+public enum Cell {
+    MISSED('X'), HIT('O'), NORMAL('?'), SHIP('?');
+    
+    private char symbol;
+
+    private Cell(char symbol) {
+        this.symbol=symbol;
+    }
+    
+    public char getSymbol(){
+        return symbol;
+    }
+}
